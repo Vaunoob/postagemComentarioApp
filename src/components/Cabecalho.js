bc4c5a21dbb0b65b959e156207dfe54112bb25af
Verners <verners_sccp@hotmail.com>
2020-09-02 21:08:37 -0300

    Corrigido Link

--- src/components/Cabecalho.js
+++ src/components/Cabecalho.js
@@ -22,9 +22,9 @@ function Cabecalho() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="/Postagem">
+              <NavLink className="nav-link" to="/Postagem">
                 Nova Postagem <span className="sr-only">(current)</span>
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
